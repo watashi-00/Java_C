a8bbd7885dcce1d301f750b8bd3c56c3d2670da2
watashi-00 <kalitajj374@gmail.com>
2026-08-18 13:18:49 -0300

    refactor: make LinkedList generic and add Node class

--- src/main/java/com/lab/dsa/list/LinkedList.java
+++ src/main/java/com/lab/dsa/list/LinkedList.java
@@ -1,5 +1,54 @@
 package src.main.java.com.lab.dsa.list;
 
-public class LinkedList {
-    
+public class LinkedList <T> {
+    Node <T> head;
+    Node <T> tail;
+
+    public LinkedList () {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public LinkedList (T headContent) {
+        this.head = new Node<>(headContent);
+        this.tail = this.head;
+    }
+
+
+    public LinkedList (T headContent, T tailContent) {
+        this.head = new Node<>(headContent);
+        this.tail = new Node<>(tailContent);
+        this.head.next = this.tail;
+    }
+
+    public LinkedList (Node<T> head) {
+        this.head = head;
+        this.tail = this.head;
+    }
+
+    public LinkedList (Node<T> head, Node<T> tail) {
+        this.head = head;
+        this.tail = tail;
+    }
+
+
+}
+
+
+class Node <T> {
+    Node <T> next;
+    private final T content;
+
+    Node (T content) {
+        this.content = content;
+        this.next = null;
+    }
+
+    public T getContent() {
+        return content;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
 }
