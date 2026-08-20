237328fc1d641f603f7a954be36fbf143903885e
watashi-00 <kalitajj374@gmail.com>
2026-08-20 18:26:07 -0300

    fix: restructure hash map logic to handle collisions and support lookup

--- src/main/java/com/lab/dsa/hashmap/HashUnrolledLinkedList.java
+++ src/main/java/com/lab/dsa/hashmap/HashUnrolledLinkedList.java
@@ -13,33 +13,31 @@ public class HashUnrolledLinkedList <K> {
     }
 
     void append(K element) {
-        int hash = element.hashCode() & (list.length - 1); // only pow of 2
+        int hash = genHash(element);
 
         if(list[hash] == null){
             list[hash] = new Linker<K>();
-            return;
         }
-
-        System.out.println("Collision");
-
+        list[hash].put(element);
+        size++;
     }
 
-    @SuppressWarnings("unchecked")
-    K get(int i) {
-        if(i >= size || i < 0){
-            throw new ArrayIndexOutOfBoundsException("Index out of bounds");
+
+    public boolean contains(K element) {
+        int hash = genHash(element);
+
+        var linker = list[hash];
+
+        if (linker == null){
+            return false;
         }
 
-        if(i <= list.length){
-            return (K) list[i];
-        }
+        return linker.exists(element);
 
-        return getFromIntern(i);
-    }
+    };
 
-    @SuppressWarnings("unchecked")
-    private K getFromIntern(int i) {
-        return  (K) list[i];
+    private int genHash(K key) {
+        return key.hashCode() & (list.length - 1);  // only pow of 2
     }
 
 }
@@ -58,22 +56,52 @@ class Linker<K> {
     }
 
     void put(K element) {
+        var curLinker = getLatestLinker(this);
 
-        var curLinker = this;
-
-        while(curLinker.usedSize == curLinker.size && curLinker.nextLinker != null) {
-            curLinker = curLinker.nextLinker;
-        }
-
-        if(usedSize == size){
+        if(curLinker.usedSize == curLinker.size){
             curLinker.nextLinker = new Linker<K>();
             curLinker = curLinker.nextLinker;
         }
 
-        curLinker.currentList[usedSize] = element;
+        curLinker.currentList[curLinker.usedSize] = element;
         curLinker.usedSize++;
 
         System.out.println("Added: " + element);
     }
 
+    boolean exists(K element) {
+        return findElement(this, element) != null;
+    }
+
+    private Linker<K> getLatestLinker(Linker<K> curLinker) {
+        while(curLinker.usedSize == curLinker.size && curLinker.nextLinker != null) {
+            curLinker = curLinker.nextLinker;
+        }
+        return curLinker;
+    }
+
+    private SearchedElement<K> findElement(Linker<K> curLinker, K element) {
+        while(curLinker != null) {
+
+            var Sel = _getElement(curLinker,  element);
+            if(Sel != null){
+                return Sel;
+            }
+            curLinker = curLinker.nextLinker;
+        };
+
+        return null;
+    }
+
+    private SearchedElement<K> _getElement(Linker<K> curLinker, K element) {
+        for(var i = 0; i < curLinker.usedSize; i++){
+            if(curLinker.currentList[i].equals(element)) { // needs override Equals on element
+                return new SearchedElement<>(curLinker, element);
+            }
+        }
+        return null;
+    }
+
 }
+
+record SearchedElement<K>(Linker<K> linker, K element ) {}
