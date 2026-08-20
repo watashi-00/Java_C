f65435bcc0564eab0134b1bf8d43dd32cc136ddb
watashi-00 <kalitajj374@gmail.com>
2026-08-20 17:45:55 -0300

    feat: implement Linker chain for unrolled hash list collision handling

--- src/main/java/com/lab/dsa/hashmap/HashUnrolledLinkedList.java
+++ src/main/java/com/lab/dsa/hashmap/HashUnrolledLinkedList.java
@@ -1,21 +1,22 @@
 package com.lab.dsa.hashmap;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Arrays;
 
 public class HashUnrolledLinkedList <K> {
-    private final Object[] list;
+    private final Linker<K>[] list;
     private int size;
-    
+
+    @SuppressWarnings("unchecked")
     HashUnrolledLinkedList() {
-        this.list = new Object[8];
+        this.list = (Linker<K>[]) new Linker[8];
+        Arrays.fill(list, null);
     }
 
     void append(K element) {
         int hash = element.hashCode() & (list.length - 1); // only pow of 2
 
         if(list[hash] == null){
-            list[hash] = element;
+            list[hash] = new Linker<K>();
             return;
         }
 
@@ -39,6 +40,40 @@ public class HashUnrolledLinkedList <K> {
     @SuppressWarnings("unchecked")
     private K getFromIntern(int i) {
         return  (K) list[i];
+    }
+
+}
+
+class Linker<K> {
+    private final Object[] currentList;
+    private final int size;
+
+    private Linker<K> nextLinker;
+    private int usedSize;
+
+    Linker() {
+        currentList = new Object[16];
+        this.usedSize = 0;
+        this.size = currentList.length;
+    }
+
+    void put(K element) {
+
+        var curLinker = this;
+
+        while(curLinker.usedSize == curLinker.size && curLinker.nextLinker != null) {
+            curLinker = curLinker.nextLinker;
+        }
+
+        if(usedSize == size){
+            curLinker.nextLinker = new Linker<K>();
+            curLinker = curLinker.nextLinker;
+        }
+
+        curLinker.currentList[usedSize] = element;
+        curLinker.usedSize++;
+
+        System.out.println("Added: " + element);
     }
 
 }
