8a6c112f8ce8aeb688b694be014ea2e715f8c64a
watashi-00 <94692745+watashi-00@users.noreply.github.com>
2026-09-03 10:00:38 -0300

    feat(lists): add LinkedListFifo class stub implementing Lab interface

---
+++ src/main/java/com/lab/dsa/lists/LinkedListFifo.java
@@ -0,0 +1,17 @@
+package com.lab.dsa.lists;
+
+import com.lab.utils.Lab;
+import com.lab.utils.MenuSelect;
+
+@MenuSelect(label = "Linked List: FIFO")
+public class LinkedListFifo implements Lab {
+
+    private Node head;
+    private Node tail;
+
+    @Override
+    public void run() {
+
+    }
+
+}
