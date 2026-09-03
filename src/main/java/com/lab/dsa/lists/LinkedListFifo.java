8917412f4026589e87f97c7c52463ee7b2ffca64
watashi-00 <94692745+watashi-00@users.noreply.github.com>
2026-09-03 10:09:09 -0300

    feat(LinkedListFifo): add append(Node) implementation and pop() method

--- src/main/java/com/lab/dsa/lists/LinkedListFifo.java
+++ src/main/java/com/lab/dsa/lists/LinkedListFifo.java
@@ -14,8 +14,40 @@ public class LinkedListFifo implements Lab {
 
     }
 
-    void append() {
+    void append(Node node) {
+        if (node == null) {
+            return;
+        }
 
+        if(head == null) {
+            this.head = node;
+            this.tail = node;
+            return;
+        }
+
+        if(this.head == this.tail) {
+            this.tail = node;
+            this.head.setNext(node);
+            return;
+        }
+
+        this.tail.setNext(node);
+        this.tail = node;
+
+    }
+
+    Node pop() {
+        if(head == null) {
+            return null;
+        }
+
+        Node temp =  this.head;
+        if(this.head == this.tail) {
+            this.tail = null;
+        }
+
+        this.head = this.head.getNext();
+        return temp;
     }
 
     public Node getHead() {
